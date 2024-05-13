a6efbe49647ca08646988f9ab55649f1ba8e2b82
eijis-pan <eijis@to2488.ddns.net>
2024-05-14 04:29:53 +0900

    ローテーションを一通り実装

---
+++ Assets/eijis/UdonScripts/BilliardsModuleAdditional/ButtonNextBallOnSpot.cs
@@ -0,0 +1,14 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class ButtonNextBallOnSpot : UdonSharpBehaviour
+{
+    [SerializeField] BilliardsModule table;
+
+    public override void Interact()
+    {
+        table._NextBallOnSpot(transform.localPosition.x);
+    }
+}
