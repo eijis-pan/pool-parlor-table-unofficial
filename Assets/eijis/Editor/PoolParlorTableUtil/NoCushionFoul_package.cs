6cbe89f51926a500aae7a04bce94e6f05f47f065
eijis-pan <eijis@to2488.ddns.net>
2024-12-07 01:56:22 +0900

    Desktopモードでトップビューが表示されない不具合を修正

--- Assets/eijis/Editor/PoolParlorTableUtil/NoCushionFoul_package.cs
+++ Assets/eijis/Editor/PoolParlorTableUtil/NoCushionFoul_package.cs
@@ -27,6 +27,7 @@ namespace EijisPoolParlorTableUtil
 			"Assets/metaphira/Modules/BilliardsModule/UdonScripts/MenuManager.cs",
 			"Assets/metaphira/Modules/BilliardsModule/UdonScripts/NetworkingManager.cs",
 			"Assets/metaphira/Modules/BilliardsModule/UdonScripts/StandardPhysicsManager.cs",
+			"Assets/metaphira/Modules/CameraOverrideModule/UdonScripts/CameraOverrideModule.cs",
 		};
 		
 		[MenuItem("GameObject/TKCH/PoolParlor/ExportPackageNoCushionFoul", false, 0)]
